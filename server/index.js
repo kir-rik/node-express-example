2d889e40816475cfb591246784726994b156b0c5
kir_rik <kir-rik-90@yandex.ru>
2020-08-30 17:00:04 +0400

    cm

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/show-get-params", (req, res) => {
+  res.send("???");
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
