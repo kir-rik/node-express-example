d0a73c3aed72b2a3e43fe3de423f37ba35f0305e
kir_rik <kir-rik-90@yandex.ru>
2020-08-30 18:54:52 +0400

    do-post

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,7 +1,18 @@
 {
-  const root = document.getElementById("root");
-  const div = document.createElement("div");
-  div.innerHTML = "Привет";
+  const button = document.getElementById("form-submit");
+  const form = document.getElementById("form");
 
-  root.appendChild(div);
+  button.addEventListener("click", async (e) => {
+    e.preventDefault();
+    fetch("/do-post", {
+      method: "POST",
+    })
+      .then((response) => response.text())
+      .then((value) => {
+        const div = document.createElement("div");
+        div.innerHTML = value;
+        form.appendChild(div);
+      })
+      .catch((e) => console.log(e));
+  });
 }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.use('/', express.static('public/pages'))
 app.use('/static', express.static('public/js'))
 app.use('/static', express.static('public/css'))
 
+app.post('/do-post', async (req, res) => {
+  res.send('post response')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
