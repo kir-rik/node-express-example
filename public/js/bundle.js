8ac825ac983ff93b7a9a682c1a3aa4bd58e620b0
kir_rik <kir-rik-90@yandex.ru>
2020-08-30 17:54:12 +0400

    public

---
+++ public/js/bundle.js
@@ -0,0 +1,7 @@
+{
+  const root = document.getElementById("root");
+  const div = document.createElement("div");
+  div.innerHTML = "Привет";
+
+  root.appendChild(div);
+}
--- server/index.js
+++ server/index.js
@@ -2,17 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/show-get-params", (req, res) => {
-  const divs = Object.keys(req.query).map(key => `<div>${key}: ${req.query[key]}</div>`)
-  const result = `<html><body>${divs.join('')}</body></html>`
-
-  res.send(result);
-});
-
+app.use('/', express.static('public/pages'))
+app.use('/static', express.static('public/js'))
+app.use('/static', express.static('public/css'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
